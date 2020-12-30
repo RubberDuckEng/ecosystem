14c057a796b310944ea759dbbb0e5a76ffc037fd
Eric Seidel <github@eseidel.com>
2020-12-30 12:51:46 -0800

    Add HUD
    Added a small HUD with counts.
    
    Also upgraded to the latest LTS Unity.

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public Text countText;
+
+    void Update()
+    {
+        // FIXME: There is probably a more efficient way to get these counts
+        // or perhaps we should update this less frequently than once per frame.
+        int preyCount = GameObject.FindGameObjectsWithTag("Prey").Length;
+        int plantCount = GameObject.FindGameObjectsWithTag("Plant").Length;
+        int predatorCount = GameObject.FindGameObjectsWithTag("Predator").Length;
+
+        countText.text = $"Predators: {predatorCount}\nPrey: {preyCount}\nPlants: {plantCount}";
+    }
+}
